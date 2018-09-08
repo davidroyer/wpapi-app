45df530a744254c34ab991035e299ae7d43206b9
David Royer <droyer01@gmail.com>
2018-09-08 02:57:26 -0400

    added function get blog_id

--- wpapi-inc/headless-redirect.php
+++ wpapi-inc/headless-redirect.php
@@ -2,6 +2,7 @@
     if ( ! function_exists( 'headless_redirect_all_to_login' ) ) {
         add_filter( 'wp', 'headless_redirect_all_to_login', 0 );
         function headless_redirect_all_to_login() {
+          $blog_id = get_current_blog_id();
           if ( !is_main_site($blog_id) && !is_admin() ) {
             header( 'HTTP/1.1 403 Forbidden' );
             header( 'Location: ' . wp_login_url() );
